e8b84049553b287d245824e7117759a4861b768b
s0rg <al3x.s0rg@gmail.com>
2016-02-23 16:55:31 +0600

    + setup.py

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import os
+import sys
+import microdbf
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist upload")
+    sys.exit()
+
+elif sys.argv[-1] == "test":
+    os.system("python3 -m unittest parser_test.py")
+    sys.exit()
+
+setup(
+    name='microdbf',
+    version=microdbf.__version__,
+    description='Tiny pure python stream-oriented dbf reader',
+    long_description=open('README.md', 'rt').read(),
+    author=microdbf.__author__,
+    author_email=microdbf.__email__,
+    package_data={'': ['LICENSE']},
+    package_dir={'microdbf': 'microdbf'},
+    packages = ['microdbf'],
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[],
+    license='MIT',
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.3',
+    ),
+)
